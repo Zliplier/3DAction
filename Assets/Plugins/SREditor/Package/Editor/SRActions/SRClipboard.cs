6b565b7f5f7b01c022eece8d8f2aa0a3e2ce9340
Zliplier <ktiranakul@gmail.com>
2026-08-17 09:42:52 +0700

    ReImport

--- Assets/Plugins/SREditor/Package/Editor/SRActions/SRClipboard.cs
+++
@@ -1,9 +0,0 @@
-namespace SerializeReferenceEditor.Editor.SRActions
-{
-    public static class SRClipboard
-    {
-        public static object ManagedReferenceValue { get; set; }
-
-        public static bool HasValue => ManagedReferenceValue != null;
-    }
-}

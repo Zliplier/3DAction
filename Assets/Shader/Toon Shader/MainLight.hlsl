776c7e7872b4d6e29b96e4fcdda3699ee399d8c2
Zliplier <ktiranakul@gmail.com>
2026-08-18 17:19:05 +0700

    DiffuseAndColorDone

--- Assets/Shader/Toon Shader/MainLight.hlsl
+++ Assets/Shader/Toon Shader/MainLight.hlsl
@@ -3,25 +3,11 @@
 
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
-#ifndef SHADERGRAPH_PREVIEW
-float3 CalculateDiffuse(Light l, float3 normal, float3 view)
-{
-    //Attenuation
-    float attenuation = l.shadowAttenuation * l.distanceAttenuation;
-    //Diffuse
-    float diffuse = saturate(dot(normal, l.direction));
-    float3 h = SafeNormalize(l.direction + view);
-    diffuse *= attenuation;
-    
-    return l.color * diffuse;
-}
-#endif
-
 void MainLight_float(float3 Position, float3 Normal, float3 View, out float3 Color, out float3 Direction, out float Attenuation)
 {
     #if defined(SHADERGRAPH_PREVIEW)
-    Color = float3(0.5f, 0.5f, 0.5f);
-    Direction = float3(0.5f, 0.5f, 0.5f);
+    Color = float3(0.5, 0.5, 0);
+    Direction = float3(0.5, 0.5, 0);
     #else
     //Calculate Shadow Coord
     #if SHADOWS_SCREEN
@@ -35,7 +21,7 @@ void MainLight_float(float3 Position, float3 Normal, float3 View, out float3 Col
     View = SafeNormalize(View);
 
     Light light = GetMainLight(shadowCoord);
-    Color = CalculateDiffuse(light, Normal, View);
+    Color = saturate(light.color);
     Direction = light.direction;
     Attenuation = light.shadowAttenuation;
     #endif
